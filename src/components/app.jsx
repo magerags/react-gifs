f7a04575845969b8c3650f9213bc0f1265a3c719
Tom Whittle <tomwhittle@hey.com>
2021-04-21 21:56:17 +0200

    added selected gif functionality. COMPLETE!

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import giphy from 'giphy-api';
 import Gif from "./gif";
 import GifList from "./gif_list";
 import SearchBar from "./search_bar";
@@ -9,21 +10,39 @@ class App extends Component {
 
     this.state = {
       gifs: [],
-      selectedGifId: ["xT9IgDEI1iZyb2wqo8"]
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
     };
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  search = (query) => {
+    giphy("ObbPdrGCfRD27mqOtF1XhHZVc8sxelJl").search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
   }
 
   render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar search={this.search} />
           <div className="selected-gif">
             <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={this.state.gifs} />
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
         </div>
       </div>
     );
---
+++ src/components/new-component.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class NewComponent extends Component {
+  render() {
+    return <input type="text" className="form-control form-search" />;
+  }
+}
+
+export default NewComponent;
